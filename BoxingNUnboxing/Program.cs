e39447e503826020c70c03908ad6709da7dee775
Hanxuan Chua <Hanxuan Chua@LAPTOP-QAI2QI4H>
2021-09-21 13:35:19 +0800

    Add project files.

---
+++ BoxingNUnboxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingNUnboxing
+{
+    class Program
+    {
+        public void add(object a, object b)
+        {
+            Console.WriteLine((int)a + (int)b);
+        }
+        static void Main(string[] args)
+        {
+            Program prg = new Program();
+            prg.add(4, 5);
+            Console.ReadLine();
+        }
+    }
+}
